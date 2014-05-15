153faa39fa6de7335903480dafcf977473875581
Luke <lukeswitzer@network-LAN.local>
2014-05-14 21:05:59 -0600

    Lost.

---
+++ Week_2/BaconNinja 2/BaconNinja 2/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  BaconNinja 2
+//
+//  Created by Luke Switzer on 5/14/14.
+//  Copyright com.lukeswitzer.baconNinja2 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Week_2/BaconNinja 2/BaconNinja 2/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BaconNinja 2
+//
+//  Created by Luke Switzer on 5/14/14.
+//  Copyright com.lukeswitzer.baconNinja2 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
