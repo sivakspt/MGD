8da17f18fdf5de18c08e7522f0d5d835956740f0
Luke <lukeswitzer@network-LAN.local>
2014-05-19 09:44:26 -0600

    Added week 3 & texture packer sprites for animation

---
+++ Week_3/Bacon Ninja 3/Bacon Ninja 3/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  Bacon Ninja 3
+//
+//  Created by Luke Switzer on 5/19/14.
+//  Copyright com.lukeswitzer.baconNinja3 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
---
+++ Week_3/Bacon Ninja 3/Bacon Ninja 3/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bacon Ninja 3
+//
+//  Created by Luke Switzer on 5/19/14.
+//  Copyright com.lukeswitzer.baconNinja3 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
